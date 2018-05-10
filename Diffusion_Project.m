21ed39d0dc7b332f48200e309a8ee352cb46c2be
chadhenry1 <chadhenry1994@hotmail.com>
2018-05-09 23:46:23 -0500

    edited final report

--- Diffusion_Project.m
+++ Diffusion_Project.m
@@ -29,11 +29,11 @@ h = (bx-ax)/(N+1);
 u = zeros(N,N);
 fb = ((by-y).^2).*cos(y.*pi/by);
 gb = (by-y).^2.*y; 
-u(1,1:N-1) = fb(1,1:N-1);    %left bound
-u(1,1:N-1) = gb(1,1:N-1);    %right bound
+u(1,:) = fb(1,:);    %left bound
+u(1,:) = gb(1,:);    %right bound
 toolong = fb(1) + ((x-ax)/(bx-ax)).*(gb(1)-fb(1));
-u(1:N-1,1) = toolong(1:N-1,1); %lower bound
+u(:,1) = toolong(:,1); %lower bound
 toolong2 = fb(N) + ((x-ax)/(bx-ax)).*(gb(N)-fb(N));
-u(1:N,N) = toolong2(1:N,N);  %made up boundary, couldnt figure out ghost node
+u(:,N) = toolong2(:,N);  %made up boundary, couldnt figure out ghost node
 
 surf(X,Y,u)
